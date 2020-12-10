cbef0997afe5b9e0a62533a2e0f5eaf266623c78
dsouz039 <dsouz039@umn.edu>
2020-12-10 13:40:05 -0600

    Add plotting to mario

--- mario.py
+++ mario.py
@@ -16,25 +16,28 @@ import gym_super_mario_bros
 from wrappers import make_env
 from networks import DQN
 from agents import Memory, MarioAgent
-from rewards import plot_rewards, save_rewards, load_rewards
+from plot_utils import plot_rewards, save_rewards, load_rewards, VisdomLinePlotter
 
 @attr.s
 class Config:
     batch_size = attr.ib(32)
-    copy_step = attr.ib(5000)
+    copy_step = attr.ib(2000)
     device = attr.ib("cuda")
-    do_load_model = attr.ib(True)
+    do_dynamic_plot = attr.ib(True)
+    do_load_model = attr.ib(False)
     exploration_decay = attr.ib(0.99)
     exploration_max = attr.ib(1.0)
-    exploration_min = attr.ib(0.02)
+    exploration_min = attr.ib(0.08)
     gamma = attr.ib(0.90)
-    is_training = attr.ib(False)
-    learning_rate = attr.ib(0.00025)
+    is_training = attr.ib(True)
+    iterative_loss_threshold = attr.ib(5e-2)
+    learning_rate = attr.ib(1e-4)
     memory_size = attr.ib(30000)
     number_of_episodes = attr.ib(5000)
 
 def run():
     config = Config()
+    plotter = VisdomLinePlotter() if config.do_dynamic_plot else None
     env = gym_super_mario_bros.make("SuperMarioBros-1-1-v0")
     env = make_env(env)
 
@@ -55,8 +58,10 @@ def run():
                        exploration_max=config.exploration_max,
                        exploration_min=config.exploration_min,
                        exploration_decay=config.exploration_decay,
+                       iterative_loss_threshold=config.iterative_loss_threshold,
                        memory=memory,
-                       device=config.device)
+                       device=config.device,
+                       plotter=plotter)
 
     max_reward = 0
     rewards = list()
@@ -69,8 +74,10 @@ def run():
 
     if not config.is_training:
         agent.exploration_rate = 0.05
+        agent.eval()
 
-    for ep_num in tqdm(range(config.number_of_episodes)):
+    wins = 0
+    for episode in tqdm(range(config.number_of_episodes)):
         state = env.reset()
         state = torch.from_numpy(state).unsqueeze(0)
         episode_reward = 0
@@ -86,16 +93,24 @@ def run():
             episode_reward += reward
             step += 1
             state = torch.from_numpy(next_state).unsqueeze(0)
-            if done:
-                break
+            if info["flag_get"]:
+                wins += 1
+        print(f"{episode_reward}")
+        if plotter:
+            plotter.plot(var_name="reward", split_name="train",
+                         title_name="Episode Reward",
+                         x=episode, y=episode_reward)
 
         rewards.append(episode_reward)
 
-        if config.is_training and (ep_num+1)%1000 == 0:
+        if config.is_training and (episode+1)%1000 == 0:
             agent.save()
             agent.memory.save()
             save_rewards(rewards)
 
+        if episode%100 == 0:
+            print(f"win rate: {wins}%")
+            wins = 0
     if config.is_training:
         agent.save()
         # agent.memory.save()
